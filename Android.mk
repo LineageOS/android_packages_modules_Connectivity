c2549b4b4e2437aca29497c29ebe0b17b6214549
Lorenzo Colitti <lorenzo@google.com>
2013-11-18 15:56:02 +0900

    DO NOT MERGE: Fix compiler warnings and enable -Wall -Werror
    Change-Id: I4eb9eb407500893a27745ed2df6da9a378ddb159

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:=clatd.c dump.c checksum.c translate.c icmp.c ipv4.c ipv6.c config.c dns64.c logging.c getaddr.c getroute.c netlink_callbacks.c netlink_msg.c setif.c setroute.c mtu.c
 
+LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
 LOCAL_C_INCLUDES := external/libnl-headers
 LOCAL_STATIC_LIBRARIES := libnl_2
 LOCAL_SHARED_LIBRARIES := libcutils liblog
