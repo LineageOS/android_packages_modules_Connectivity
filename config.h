baa3c6aefa73bec1ea193c001cc8f516ecb6ef3f
Lorenzo Colitti <lorenzo@google.com>
2020-06-02 01:55:12 +0900

    Delete the clatd code that assigns IPv4 and IPv6 addresses.
    The IPv4 and IPv6 address assignment code, and its tests, were
    migrated to ClatdController in Q, and this code has been unused
    since then.
    
    This code is not only unused, it also *should* never be used,
    because if it were, many parts of 464xlat on Android would not
    work as expected. This includes 464xlat BPF offload and data
    usage accounting, both of which require netd to know exactly
    which IPv4 and IPv6 addresses are used by clatd at any given
    time.
    
    Most of the code is deleted, but the function to generate random
    checksum-neutral IIDs was moved to the tests instead.
    
    This change does not delete the configuration file parsing code.
    That will be done in a follow-up CL.
    
    Bug: 144730808
    Bug: 150738490
    Test: atest clatd_test
    Test: atest netd_integration_test
    Test: IPv6-only wifi continues to work
    Change-Id: I8265e9bb04d44ed1dda445fa91881fcd1601dc47

--- config.h
+++ config.h
@@ -37,10 +37,6 @@ struct clat_config {
 extern struct clat_config Global_Clatd_Config;
 
 int read_config(const char *file, const char *uplink_interface);
-void config_generate_local_ipv6_subnet(struct in6_addr *interface_ip);
-in_addr_t config_select_ipv4_address(const struct in_addr *ip, int16_t prefixlen);
 int ipv6_prefix_equal(struct in6_addr *a1, struct in6_addr *a2);
-
-typedef int (*addr_free_func)(in_addr_t addr);
 
 #endif /* __CONFIG_H__ */
