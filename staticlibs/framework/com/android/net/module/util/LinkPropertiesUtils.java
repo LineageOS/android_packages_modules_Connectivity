94912a1b0acb48f72e7182be712cd3748735777b
Chalard Jean <jchalard@google.com>
2020-06-26 18:34:52 +0900

    Move util classes to their destination package
    Test: FrameworksWifiTests FrameworksNetTests
    Change-Id: Id19c650246a8f3d55d7b6a874840a6eebcd9eb17
    Merged-In: Id19c650246a8f3d55d7b6a874840a6eebcd9eb17

--- staticlibs/framework/com/android/net/module/util/LinkPropertiesUtils.java
+++ staticlibs/framework/com/android/net/module/util/LinkPropertiesUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.net.util;
+package com.android.net.module.util;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
