aeb54b3cf8d17db1d9cbaf54163e0e4e37878f12
Remi NGUYEN VAN <reminv@google.com>
2021-04-22 15:49:20 +0900

    Fix testSocketKeepaliveLimitTelephony on R
    DEVICE_INITIAL_SDK_INT does not exist in older SDKs: use the pre-rename
    field, FIRST_SDK_INT instead.
    
    Fixes: 185731191
    Test: atest ConnectivityManagerTest#testSocketKeepaliveLimitTelephony
    Change-Id: I1fbc0f714d539a664c053cb9cf1f86c3e651c5a2
    Merged-In: I1fbc0f714d539a664c053cb9cf1f86c3e651c5a2

--- tests/cts/net/src/android/net/cts/ConnectivityManagerTest.java
+++ tests/cts/net/src/android/net/cts/ConnectivityManagerTest.java
@@ -159,6 +159,7 @@ import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.android.internal.util.ArrayUtils;
+import com.android.modules.utils.build.SdkLevel;
 import com.android.networkstack.apishim.ConnectivityManagerShimImpl;
 import com.android.networkstack.apishim.ConstantsShim;
 import com.android.networkstack.apishim.NetworkInformationShimImpl;
@@ -1697,7 +1698,10 @@ public class ConnectivityManagerTest {
             return;
         }
 
-        final int firstSdk = Build.VERSION.DEVICE_INITIAL_SDK_INT;
+        final int firstSdk = SdkLevel.isAtLeastS()
+                ? Build.VERSION.DEVICE_INITIAL_SDK_INT
+                // FIRST_SDK_INT was a @TestApi field renamed to DEVICE_INITIAL_SDK_INT in S
+                : Build.VERSION.class.getField("FIRST_SDK_INT").getInt(null);
         if (firstSdk < Build.VERSION_CODES.Q) {
             Log.i(TAG, "testSocketKeepaliveLimitTelephony: skip test for devices launching"
                     + " before Q: " + firstSdk);
