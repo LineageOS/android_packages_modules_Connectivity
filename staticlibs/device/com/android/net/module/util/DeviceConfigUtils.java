426e1a1c4c1dd2c13a521770be103d53661c3123
Mark Chien <markchien@google.com>
2021-05-18 03:37:13 +0000

    Add isFeatureEnabled by checking with apex module version
    A variable of #isFeatureEnabled which check apex module version
    instead of apk version.
    
    Bug: 187946226
    Test: NetworkStaticLibTests
    
    Original change: https://android-review.googlesource.com/c/platform/frameworks/libs/net/+/1705174
    
    Ignore-AOSP-First: manual cherry-pick from AOSP build automerge is broken
    Change-Id: I68659e43d7fdf40678ceb9df2ff10d480a5d08e0

--- staticlibs/device/com/android/net/module/util/DeviceConfigUtils.java
+++ staticlibs/device/com/android/net/module/util/DeviceConfigUtils.java
@@ -17,6 +17,7 @@
 package com.android.net.module.util;
 
 import android.content.Context;
+import android.content.pm.ModuleInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.provider.DeviceConfig;
@@ -38,6 +39,7 @@ public final class DeviceConfigUtils {
     @VisibleForTesting
     public static void resetPackageVersionCacheForTest() {
         sPackageVersion = -1;
+        sModuleVersion = -1;
     }
 
     private static volatile long sPackageVersion = -1;
@@ -155,15 +157,88 @@ public final class DeviceConfigUtils {
     public static boolean isFeatureEnabled(@NonNull Context context, @NonNull String namespace,
             @NonNull String name, boolean defaultEnabled) {
         try {
-            final int propertyVersion = getDeviceConfigPropertyInt(namespace, name,
-                    0 /* default value */);
             final long packageVersion = getPackageVersion(context);
-            return (propertyVersion == 0 && defaultEnabled)
-                    || (propertyVersion != 0 && packageVersion >= (long) propertyVersion);
+            return isFeatureEnabled(context, packageVersion, namespace, name, defaultEnabled);
         } catch (PackageManager.NameNotFoundException e) {
             Log.e(TAG, "Could not find the package name", e);
             return false;
         }
+    }
+
+    /**
+     * Check whether or not one specific experimental feature for a particular namespace from
+     * {@link DeviceConfig} is enabled by comparing module package version
+     * with current version of property. If this property version is valid, the corresponding
+     * experimental feature would be enabled, otherwise disabled.
+     *
+     * This is useful to ensure that if a module install is rolled back, flags are not left fully
+     * rolled out on a version where they have not been well tested.
+     * @param context The global context information about an app environment.
+     * @param namespace The namespace containing the property to look up.
+     * @param name The name of the property to look up.
+     * @param moduleName The mainline module name which is released as apex.
+     * @param defaultEnabled The value to return if the property does not exist or its value is
+     *                       null.
+     * @return true if this feature is enabled, or false if disabled.
+     */
+    public static boolean isFeatureEnabled(@NonNull Context context, @NonNull String namespace,
+            @NonNull String name, @NonNull String moduleName, boolean defaultEnabled) {
+        try {
+            final long packageVersion = getModuleVersion(context, moduleName);
+            return isFeatureEnabled(context, packageVersion, namespace, name, defaultEnabled);
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Could not find the module name", e);
+            return false;
+        }
+    }
+
+    private static boolean maybeUseFixedPackageVersion(@NonNull Context context) {
+        final String packageName = context.getPackageName();
+        if (packageName == null) return false;
+
+        return packageName.equals("com.android.networkstack.tethering")
+                || packageName.equals("com.android.networkstack.tethering.inprocess");
+    }
+
+    private static boolean isFeatureEnabled(@NonNull Context context, long packageVersion,
+            @NonNull String namespace, String name, boolean defaultEnabled)
+            throws PackageManager.NameNotFoundException {
+        final int propertyVersion = getDeviceConfigPropertyInt(namespace, name,
+                0 /* default value */);
+        return (propertyVersion == 0 && defaultEnabled)
+                || (propertyVersion != 0 && packageVersion >= (long) propertyVersion);
+    }
+
+    private static volatile long sModuleVersion = -1;
+    @VisibleForTesting public static long FIXED_PACKAGE_VERSION = 10;
+    private static long getModuleVersion(@NonNull Context context, @NonNull String moduleName)
+            throws PackageManager.NameNotFoundException {
+        if (sModuleVersion >= 0) return sModuleVersion;
+
+        final PackageManager packageManager = context.getPackageManager();
+        ModuleInfo module;
+        try {
+            module = packageManager.getModuleInfo(
+                    moduleName, PackageManager.MODULE_APEX_NAME);
+        } catch (PackageManager.NameNotFoundException e) {
+            // The error may happen if mainline module meta data is not installed e.g. there are
+            // no meta data configuration in AOSP build. To be able to enable a feature in AOSP
+            // by setting a flag via ADB for example. set a small non-zero fixed number for
+            // comparing.
+            if (maybeUseFixedPackageVersion(context)) {
+                sModuleVersion = FIXED_PACKAGE_VERSION;
+                return FIXED_PACKAGE_VERSION;
+            } else {
+                throw e;
+            }
+        }
+        String modulePackageName = module.getPackageName();
+        if (modulePackageName == null) throw new PackageManager.NameNotFoundException(moduleName);
+        final long version = packageManager.getPackageInfo(modulePackageName,
+                PackageManager.MATCH_APEX).getLongVersionCode();
+        sModuleVersion = version;
+
+        return version;
     }
 
     /**
