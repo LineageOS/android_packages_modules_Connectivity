d99cf6fe74b9fb1c3f473193d98ae83cded8b559
Xiao Ma <xiaom@google.com>
2021-07-30 04:30:40 +0000

    Add data structures to parse netlink link messages.
    Bug: 163492391
    Test: atest NetworkStaticLibsTests
    Change-Id: Ida8c22a782eeaca738487d0dc041a459ef6bb936

--- staticlibs/device/com/android/net/module/util/netlink/NetlinkMessage.java
+++ staticlibs/device/com/android/net/module/util/netlink/NetlinkMessage.java
@@ -120,6 +120,9 @@ public class NetlinkMessage {
     private static NetlinkMessage parseRtMessage(@NonNull StructNlMsgHdr nlmsghdr,
             @NonNull ByteBuffer byteBuffer) {
         switch (nlmsghdr.nlmsg_type) {
+            case NetlinkConstants.RTM_NEWLINK:
+            case NetlinkConstants.RTM_DELLINK:
+                return (NetlinkMessage) RtNetlinkLinkMessage.parse(nlmsghdr, byteBuffer);
             case NetlinkConstants.RTM_NEWNEIGH:
             case NetlinkConstants.RTM_DELNEIGH:
             case NetlinkConstants.RTM_GETNEIGH:
