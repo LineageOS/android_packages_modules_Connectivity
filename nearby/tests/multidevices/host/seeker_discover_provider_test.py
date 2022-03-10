614792f123bba675181cfe9b070c02a9b5382f56
Eric Lin <ericth@google.com>
2022-03-10 10:14:05 +0000

    Refactor test scripts.
    Move helper classes to test_helper folder. Only test case files
    in the first level.
    
    Test: atest -v CtsSeekerDiscoverProviderTest
    BUG: 214015364
    Change-Id: I088adcb647d01ac558ce7dd1fea880821d59028e

--- nearby/tests/multidevices/host/seeker_discover_provider_test.py
+++ nearby/tests/multidevices/host/seeker_discover_provider_test.py
@@ -1,16 +1,28 @@
-# Lint as: python3
+#  Copyright (C) 2022 The Android Open Source Project
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 """CTS-V Nearby Mainline Fast Pair end-to-end test case: seeker can discover the provider."""
 
 import logging
 import sys
 
-from mobly import asserts
 from mobly import base_test
 from mobly import test_runner
 from mobly.controllers import android_device
 
-import fast_pair_provider_simulator
-import fast_pair_seeker
+from test_helper import fast_pair_provider_simulator
+from test_helper import fast_pair_seeker
 
 # Default model ID to simulate on provider side.
 DEFAULT_MODEL_ID = '00000C'
