6e44855f90e05d8bab82da864f3ba1db40e69594
Xiao Ma <xiaom@google.com>
2022-03-17 05:57:25 +0000

    Add getInterfaceList API implementation in Ethernet service.
    Bug: 171872016
    Test: m
    Merged-In: I8eeb2cd211c6a2ec6bc997c5e18995b585c6118a
    Change-Id: Ic36d26be7dae5fd3f72abce3cea1ee845813a6e5

--- service-t/src/com/android/server/ethernet/EthernetServiceImpl.java
+++ service-t/src/com/android/server/ethernet/EthernetServiceImpl.java
@@ -41,6 +41,7 @@ import com.android.net.module.util.PermissionUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -288,5 +289,11 @@ public class EthernetServiceImpl extends IEthernetManager.Stub {
                 android.Manifest.permission.NETWORK_SETTINGS);
 
         mTracker.setEthernetEnabled(enabled);
+    }
+
+    @Override
+    public List<String> getInterfaceList() {
+        PermissionUtils.enforceAccessNetworkStatePermission(mContext, TAG);
+        return mTracker.getInterfaceList();
     }
 }
