c67de5ccb026fe2da9f4bfa3134b3b1c9c5c4e4a
Eric Lin <ericth@google.com>
2022-03-29 04:08:56 +0000

    Fix snippet part for Mainline Fast Pair API changes.
    In ag/17397190, removed unused system apis.
    
    Test: atest -v CtsNearbyMultiDevicesTestSuite
    Bug: 204780849
    Ignore-AOSP-First: nearby_not_in_aosp_yet
    Change-Id: I36e2a30a43d53b4730b8f8c5ae7a75b2ad47ba9e

--- nearby/tests/multidevices/clients/test_service/fastpair_seeker_data_provider/shared/android/nearby/fastpair/seeker/FastPairTestDataCache.kt
+++ nearby/tests/multidevices/clients/test_service/fastpair_seeker_data_provider/shared/android/nearby/fastpair/seeker/FastPairTestDataCache.kt
@@ -109,31 +109,21 @@ class FastPairTestDataCache {
     }
 
     data class FastPairDeviceMetadataData(
-        @SerializedName("assistant_setup_half_sheet") val assistantSetupHalfSheet: String?,
-        @SerializedName("assistant_setup_notification") val assistantSetupNotification: String?,
         @SerializedName("ble_tx_power") val bleTxPower: Int,
-        @SerializedName("confirm_pin_description") val confirmPinDescription: String?,
-        @SerializedName("confirm_pin_title") val confirmPinTitle: String?,
         @SerializedName("connect_success_companion_app_installed") val compAppInstalled: String?,
         @SerializedName("connect_success_companion_app_not_installed") val comAppNotIns: String?,
         @SerializedName("device_type") val deviceType: Int,
         @SerializedName("download_companion_app_description") val downloadComApp: String?,
         @SerializedName("fail_connect_go_to_settings_description") val failConnectDes: String?,
-        @SerializedName("fast_pair_tv_connect_device_no_account_description") val accDes: String?,
         @SerializedName("image_url") val imageUrl: String?,
         @SerializedName("initial_notification_description") val initNotification: String?,
         @SerializedName("initial_notification_description_no_account") val initNoAccount: String?,
         @SerializedName("initial_pairing_description") val initialPairingDescription: String?,
         @SerializedName("intent_uri") val intentUri: String?,
-        @SerializedName("locale") val locale: String?,
         @SerializedName("name") val name: String?,
         @SerializedName("open_companion_app_description") val openCompanionAppDescription: String?,
         @SerializedName("retroactive_pairing_description") val retroactivePairingDes: String?,
         @SerializedName("subsequent_pairing_description") val subsequentPairingDescription: String?,
-        @SerializedName("sync_contacts_description") val syncContactsDescription: String?,
-        @SerializedName("sync_contacts_title") val syncContactsTitle: String?,
-        @SerializedName("sync_sms_description") val syncSmsDescription: String?,
-        @SerializedName("sync_sms_title") val syncSmsTitle: String?,
         @SerializedName("trigger_distance") val triggerDistance: Double,
         @SerializedName("case_url") val trueWirelessImageUrlCase: String?,
         @SerializedName("left_bud_url") val trueWirelessImageUrlLeftBud: String?,
@@ -144,31 +134,21 @@ class FastPairTestDataCache {
         @SerializedName("wait_launch_companion_app_description") val waitLaunchCompApp: String?
     ) {
         constructor(meta: FastPairDeviceMetadata) : this(
-            assistantSetupHalfSheet = meta.assistantSetupHalfSheet,
-            assistantSetupNotification = meta.assistantSetupNotification,
             bleTxPower = meta.bleTxPower,
-            confirmPinDescription = meta.confirmPinDescription,
-            confirmPinTitle = meta.confirmPinTitle,
             compAppInstalled = meta.connectSuccessCompanionAppInstalled,
             comAppNotIns = meta.connectSuccessCompanionAppNotInstalled,
             deviceType = meta.deviceType,
             downloadComApp = meta.downloadCompanionAppDescription,
             failConnectDes = meta.failConnectGoToSettingsDescription,
-            accDes = meta.fastPairTvConnectDeviceNoAccountDescription,
             imageUrl = meta.imageUrl,
             initNotification = meta.initialNotificationDescription,
             initNoAccount = meta.initialNotificationDescriptionNoAccount,
             initialPairingDescription = meta.initialPairingDescription,
             intentUri = meta.intentUri,
-            locale = meta.locale,
             name = meta.name,
             openCompanionAppDescription = meta.openCompanionAppDescription,
             retroactivePairingDes = meta.retroactivePairingDescription,
             subsequentPairingDescription = meta.subsequentPairingDescription,
-            syncContactsDescription = meta.syncContactsDescription,
-            syncContactsTitle = meta.syncContactsTitle,
-            syncSmsDescription = meta.syncSmsDescription,
-            syncSmsTitle = meta.syncSmsTitle,
             triggerDistance = meta.triggerDistance.toDouble(),
             trueWirelessImageUrlCase = meta.trueWirelessImageUrlCase,
             trueWirelessImageUrlLeftBud = meta.trueWirelessImageUrlLeftBud,
@@ -181,31 +161,21 @@ class FastPairTestDataCache {
 
         fun toFastPairDeviceMetadata(): FastPairDeviceMetadata {
             return FastPairDeviceMetadata.Builder()
-                .setAssistantSetupHalfSheet(assistantSetupHalfSheet)
-                .setAssistantSetupNotification(assistantSetupNotification)
                 .setBleTxPower(bleTxPower)
-                .setConfirmPinDescription(confirmPinDescription)
-                .setConfirmPinTitle(confirmPinTitle)
                 .setConnectSuccessCompanionAppInstalled(compAppInstalled)
                 .setConnectSuccessCompanionAppNotInstalled(comAppNotIns)
                 .setDeviceType(deviceType)
                 .setDownloadCompanionAppDescription(downloadComApp)
                 .setFailConnectGoToSettingsDescription(failConnectDes)
-                .setFastPairTvConnectDeviceNoAccountDescription(accDes)
                 .setImageUrl(imageUrl)
                 .setInitialNotificationDescription(initNotification)
                 .setInitialNotificationDescriptionNoAccount(initNoAccount)
                 .setInitialPairingDescription(initialPairingDescription)
                 .setIntentUri(intentUri)
-                .setLocale(locale)
                 .setName(name)
                 .setOpenCompanionAppDescription(openCompanionAppDescription)
                 .setRetroactivePairingDescription(retroactivePairingDes)
                 .setSubsequentPairingDescription(subsequentPairingDescription)
-                .setSyncContactsDescription(syncContactsDescription)
-                .setSyncContactsTitle(syncContactsTitle)
-                .setSyncSmsDescription(syncSmsDescription)
-                .setSyncSmsTitle(syncSmsTitle)
                 .setTriggerDistance(triggerDistance.toFloat())
                 .setTrueWirelessImageUrlCase(trueWirelessImageUrlCase)
                 .setTrueWirelessImageUrlLeftBud(trueWirelessImageUrlLeftBud)
@@ -222,24 +192,15 @@ class FastPairTestDataCache {
         @SerializedName("action_url") val actionUrl: String?,
         @SerializedName("action_url_type") val actionUrlType: Int,
         @SerializedName("app_name") val appName: String?,
-        @SerializedName("attachment_type") val attachmentType: Int,
         @SerializedName("authentication_public_key_secp256r1") val authenticationPublicKey: String?,
-        @SerializedName("ble_record_bytes") val bleRecordBytes: String?,
-        @SerializedName("debug_category") val debugCategory: Int,
-        @SerializedName("debug_message") val debugMessage: String?,
         @SerializedName("description") val description: String?,
         @SerializedName("device_name") val deviceName: String?,
         @SerializedName("display_url") val displayUrl: String?,
-        @SerializedName("entity_id") val entityId: String?,
-        @SerializedName("feature_graphic_url") val featureGraphicUrl: String?,
         @SerializedName("first_observation_timestamp_millis") val firstObservationMs: Long,
-        @SerializedName("group_id") val groupId: String?,
         @SerializedName("icon_fife_url") val iconFfeUrl: String?,
         @SerializedName("icon_png") val iconPng: String?,
         @SerializedName("id") val id: String?,
         @SerializedName("last_observation_timestamp_millis") val lastObservationMs: Long,
-        @SerializedName("last_user_experience") val lastUserExperience: Int,
-        @SerializedName("lost_millis") val lostMillis: Long,
         @SerializedName("mac_address") val macAddress: String?,
         @SerializedName("package_name") val packageName: String?,
         @SerializedName("pending_app_install_timestamp_millis") val pendingAppInstallMs: Long,
@@ -247,31 +208,21 @@ class FastPairTestDataCache {
         @SerializedName("state") val state: Int,
         @SerializedName("title") val title: String?,
         @SerializedName("trigger_id") val triggerId: String?,
-        @SerializedName("tx_power") val txPower: Int,
-        @SerializedName("type") val type: Int
+        @SerializedName("tx_power") val txPower: Int
     ) {
         constructor(item: FastPairDiscoveryItem) : this(
             actionUrl = item.actionUrl,
             actionUrlType = item.actionUrlType,
             appName = item.appName,
-            attachmentType = item.attachmentType,
             authenticationPublicKey = item.authenticationPublicKeySecp256r1?.base64Encode(),
-            bleRecordBytes = item.bleRecordBytes?.base64Encode(),
-            debugCategory = item.debugCategory,
-            debugMessage = item.debugMessage,
             description = item.description,
             deviceName = item.deviceName,
             displayUrl = item.displayUrl,
-            entityId = item.entityId,
-            featureGraphicUrl = item.featureGraphicUrl,
             firstObservationMs = item.firstObservationTimestampMillis,
-            groupId = item.groupId,
             iconFfeUrl = item.iconFfeUrl,
             iconPng = item.iconPng?.base64Encode(),
             id = item.id,
             lastObservationMs = item.lastObservationTimestampMillis,
-            lastUserExperience = item.lastUserExperience,
-            lostMillis = item.lostMillis,
             macAddress = item.macAddress,
             packageName = item.packageName,
             pendingAppInstallMs = item.pendingAppInstallTimestampMillis,
@@ -279,8 +230,7 @@ class FastPairTestDataCache {
             state = item.state,
             title = item.title,
             triggerId = item.triggerId,
-            txPower = item.txPower,
-            type = item.type
+            txPower = item.txPower
         )
 
         fun toFastPairDiscoveryItem(): FastPairDiscoveryItem {
@@ -288,24 +238,15 @@ class FastPairTestDataCache {
                 .setActionUrl(actionUrl)
                 .setActionUrlType(actionUrlType)
                 .setAppName(appName)
-                .setAttachmentType(attachmentType)
                 .setAuthenticationPublicKeySecp256r1(authenticationPublicKey?.base64Decode())
-                .setBleRecordBytes(bleRecordBytes?.base64Decode())
-                .setDebugCategory(debugCategory)
-                .setDebugMessage(debugMessage)
                 .setDescription(description)
                 .setDeviceName(deviceName)
                 .setDisplayUrl(displayUrl)
-                .setEntityId(entityId)
-                .setFeatureGraphicUrl(featureGraphicUrl)
                 .setFirstObservationTimestampMillis(firstObservationMs)
-                .setGroupId(groupId)
                 .setIconFfeUrl(iconFfeUrl)
                 .setIconPng(iconPng?.base64Decode())
                 .setId(id)
                 .setLastObservationTimestampMillis(lastObservationMs)
-                .setLastUserExperience(lastUserExperience)
-                .setLostMillis(lostMillis)
                 .setMacAddress(macAddress)
                 .setPackageName(packageName)
                 .setPendingAppInstallTimestampMillis(pendingAppInstallMs)
@@ -314,7 +255,6 @@ class FastPairTestDataCache {
                 .setTitle(title)
                 .setTriggerId(triggerId)
                 .setTxPower(txPower)
-                .setType(type)
                 .build()
         }
     }
