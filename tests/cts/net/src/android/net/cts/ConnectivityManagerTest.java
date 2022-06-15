716d91e6d44983acb80fc9701ecd75497f40d258
Remi NGUYEN VAN <reminv@google.com>
2022-06-15 02:20:42 +0000

    Revert "Ignore new test until prebuilts are updated."
    This reverts commit e14433a80e17bed4ddc6149f49fc0a67d1c99cdf.
    
    Reason for revert: Prebuilts are updated and test should pass
    Merged-In: I60d5540821abcced03356f366775f16ee369d7f9
    
    Change-Id: I5101bf71b3290933fc1ecf83a00b7d2080955242

--- tests/cts/net/src/android/net/cts/ConnectivityManagerTest.java
+++ tests/cts/net/src/android/net/cts/ConnectivityManagerTest.java
@@ -199,7 +199,6 @@ import libcore.io.Streams;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -3415,7 +3414,6 @@ public class ConnectivityManagerTest {
         }, NETWORK_SETTINGS);
     }
 
-    @Ignore("TODO: temporarily ignore tests until prebuilts are updated")
     @Test @IgnoreUpTo(SC_V2)
     public void testFirewallBlocking() {
         // Following tests affect the actual state of networking on the device after the test.
