9518062a25a52aabf27438d29113364588178be8
Xinyi Zhou <zxy@google.com>
2022-07-11 22:49:54 -0700

    Add Cryptor interface
    Test: -m and loaded on device
    Bug: 232847868
    Ignore-AOSP-First: nearby_not_in_aosp_yet
    Change-Id: Ia623b56e3ae07c8e899c766463c9a3d32128870a

---
+++ nearby/service/java/com/android/server/nearby/util/encryption/Cryptor.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.nearby.util.encryption;
+
+import androidx.annotation.Nullable;
+
+/** Interface for encryption/decryption functionality. */
+public interface Cryptor {
+
+    /**
+     * Encrypt the provided data blob.
+     *
+     * @param data data blob to be encrypted.
+     * @param salt used for IV
+     * @param secretKeyBytes secrete key accessed from credentials
+     * @return encrypted data, {@code null} if failed to encrypt.
+     */
+    @Nullable
+    byte[] encrypt(byte[] data, byte[] salt, byte[] secretKeyBytes);
+
+    /**
+     * Decrypt the original data blob from the provided byte array.
+     *
+     * @param encryptedData data blob to be decrypted.
+     * @param salt used for IV
+     * @param secretKeyBytes secrete key accessed from credentials
+     * @return decrypted data, {@code null} if failed to decrypt.
+     */
+    @Nullable
+    byte[] decrypt(byte[] encryptedData, byte[] salt, byte[] secretKeyBytes);
+}
