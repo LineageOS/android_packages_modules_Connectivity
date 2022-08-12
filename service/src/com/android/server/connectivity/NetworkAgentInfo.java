00955f3d46c4b0965ab3b0a7984bfbdcfa4db070
Paul Hu <paulhu@google.com>
2022-08-12 06:45:00 +0000

    Check automotive system feature on Constructor
    The system feature depends on device, it will not change at runtime
    basically. So do the system feature check once on NetworkAgentInfo
    constructor.
    
    Test: FrameworksNetTests CtsNetTestCases
    Change-Id: I03fb8b87f5fda93286bd68ccbb23210066b0611b

--- service/src/com/android/server/connectivity/NetworkAgentInfo.java
+++ service/src/com/android/server/connectivity/NetworkAgentInfo.java
@@ -392,6 +392,9 @@ public class NetworkAgentInfo implements NetworkRanker.Scoreable {
     // URL, Terms & Conditions URL, and network friendly name.
     public CaptivePortalData networkAgentPortalData;
 
+    // Indicate whether this device has the automotive feature.
+    private final boolean mHasAutomotiveFeature;
+
     /**
      * Sets the capabilities sent by the agent for later retrieval.
      *
@@ -433,9 +436,8 @@ public class NetworkAgentInfo implements NetworkRanker.Scoreable {
                     + networkCapabilities.getOwnerUid() + " to " + nc.getOwnerUid());
             nc.setOwnerUid(networkCapabilities.getOwnerUid());
         }
-        restrictCapabilitiesFromNetworkAgent(nc, creatorUid,
-                mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE),
-                carrierPrivilegeAuthenticator);
+        restrictCapabilitiesFromNetworkAgent(
+                nc, creatorUid, mHasAutomotiveFeature, carrierPrivilegeAuthenticator);
         return nc;
     }
 
@@ -604,6 +606,8 @@ public class NetworkAgentInfo implements NetworkRanker.Scoreable {
                 ? nc.getUnderlyingNetworks().toArray(new Network[0])
                 : null;
         mCreationTime = System.currentTimeMillis();
+        mHasAutomotiveFeature =
+                mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE);
     }
 
     private class AgentDeathMonitor implements IBinder.DeathRecipient {
