01ca15d6f4560330c2cb836e7892f5fa58e6e139
Tommy Webb <tommy@calyxinstitute.org>
2022-09-20 17:12:48 +0000

    Use BPF to block socket creation when restricted
    Adapt BPF's inet socket creation rule to not only check INTERNET
    permission but also to ensure the uid is on the allowlist for
    restricted networking (has RESTRICTED_MATCH flag).
    
    Also includes squashed change:
    
    Author: Tommy Webb <tommy@calyxinstitute.org>
    Date:   Thu Sep 7 03:59:21 2023 -0400
    
        Deny socket creation for transport-blocked apps
    
        Prevent apps whose network access is blocked based on transport
        policies from creating sockets, too. Update the logic to match AOSP's
        "funky bit-wise arithmetic" from the latest Connectivity mainline.
    
        Test: Manual: Turn on Private DNS. Install Terminal Emulator. Connect
        to Wi-Fi (no VPN). Set Terminal Emulator's toggles to disable Wi-Fi.
        Run: `ping duckduckgo.com`. Should receive "unknown host" error, NOT
        "Network is unreachable". Same "unknown host" error should occur when
        testing with overall network access turned off for Terminal Emulator,
        with and without its Wi-Fi access also turned off.
    
        Issue: calyxos#581
        Change-Id: I995e9929f6f8c1ae0613e05e0cade55a76c35902
    
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Change-Id: I912a4a2ee78a29ca8b7d8ff85e5ad7cf617c31a5

--- bpf/progs/netd.h
+++ bpf/progs/netd.h
@@ -386,6 +386,9 @@ STRUCT_SIZE(SkbIpPacketData, 16 + 16 + 2 + 2 + 1 + 1 + 1 + 1); // 40
 #define DROP_IF_UNSET (DOZABLE_MATCH | POWERSAVE_MATCH | RESTRICTED_MATCH \
                         | LOW_POWER_STANDBY_MATCH | BACKGROUND_MATCH)
 
+#define FIREWALL_DROP_IF_SET (OEM_DENY_1_MATCH)
+#define FIREWALL_DROP_IF_UNSET (RESTRICTED_MATCH)
+
 // Warning: funky bit-wise arithmetic: in parallel, for all DROP_IF_SET/UNSET rules
 // check whether the rules are globally enabled, and if so whether the rules are
 // set/unset for the specific uid.  DROP if that is the case for ANY of the rules.
