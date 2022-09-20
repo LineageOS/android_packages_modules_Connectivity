14b3cb9dd0fc43b8de996ac2ab8b9bdf0080dc8b
Tommy Webb <tommy@calyxinstitute.org>
2022-09-20 17:12:48 +0000

    Use BPF to block socket creation when restricted
    Adapt BPF's inet socket creation rule to not only check INTERNET
    permission but also to ensure the uid is on the allowlist for
    restricted networking (has RESTRICTED_MATCH flag).
    
    Also includes squashed change:
    
    Author: Tommy Webb <tommy@calyxinstitute.org>
    Date:   Thu Sep 7 03:59:21 2023 -0400
    
        Deny socket creation for transport-blocked apps
    
        Prevent apps whose network access is blocked based on transport
        policies from creating sockets, too. Update the logic to match AOSP's
        "funky bit-wise arithmetic" from the latest Connectivity mainline.
    
        Test: Manual: Turn on Private DNS. Install Terminal Emulator. Connect
        to Wi-Fi (no VPN). Set Terminal Emulator's toggles to disable Wi-Fi.
        Run: `ping duckduckgo.com`. Should receive "unknown host" error, NOT
        "Network is unreachable". Same "unknown host" error should occur when
        testing with overall network access turned off for Terminal Emulator,
        with and without its Wi-Fi access also turned off.
    
        Issue: calyxos#581
        Change-Id: I995e9929f6f8c1ae0613e05e0cade55a76c35902
    
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Change-Id: I912a4a2ee78a29ca8b7d8ff85e5ad7cf617c31a5

--- bpf/progs/netd.c
+++ bpf/progs/netd.c
@@ -460,6 +460,21 @@ static __always_inline inline bool ingress_should_discard(struct __sk_buff* skb,
     return true;  // disallowed interface
 }
 
+static __always_inline inline int bpf_owner_firewall_match(uint32_t uid) {
+    if (is_system_uid(uid)) return PASS;
+
+    const BpfConfig enabledRules = getConfig(UID_RULES_CONFIGURATION_KEY);
+    const UidOwnerValue* uidEntry = bpf_uid_owner_map_lookup_elem(&uid);
+    const uint32_t uidRules = uidEntry ? uidEntry->rule : 0;
+
+    if (enabledRules & (FIREWALL_DROP_IF_SET | FIREWALL_DROP_IF_UNSET)
+            & (uidRules ^ FIREWALL_DROP_IF_UNSET)) {
+        return DROP;
+    }
+
+    return PASS;
+}
+
 static __always_inline inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid,
                                                   const struct egress_bool egress,
                                                   const struct kver_uint kver,
@@ -756,15 +771,14 @@ DEFINE_XTBPF_PROG("skfilter/denylist/xtbpf", xt_bpf_denylist_prog)
     return XTBPF_NOMATCH;
 }
 
-static __always_inline inline uint8_t get_app_permissions() {
-    uint64_t gid_uid = bpf_get_current_uid_gid();
+static __always_inline inline uint8_t get_app_permissions(uint32_t uid) {
     /*
      * A given app is guaranteed to have the same app ID in all the profiles in
      * which it is installed, and install permission is granted to app for all
      * user at install time so we only check the appId part of a request uid at
      * run time. See UserHandle#isSameApp for detail.
      */
-    uint32_t appId = (gid_uid & 0xffffffff) % AID_USER_OFFSET;  // == PER_USER_RANGE == 100000
+    uint32_t appId = uid % AID_USER_OFFSET;  // == PER_USER_RANGE == 100000
     uint8_t* permissions = bpf_uid_permission_map_lookup_elem(&appId);
     // if UID not in map, then default to just INTERNET permission.
     return permissions ? *permissions : BPF_PERMISSION_INTERNET;
@@ -772,7 +786,12 @@ static __always_inline inline uint8_t get_app_permissions() {
 
 DEFINE_NETD_BPF_PROG_KVER("cgroupsock/inet_create", inet_socket_create, KVER_4_14)
 (__unused struct bpf_sock* sk) {
-    return (get_app_permissions() & BPF_PERMISSION_INTERNET) ? BPF_ALLOW : BPF_DISALLOW;
+    uint64_t uid = bpf_get_current_uid_gid() & 0xffffffff;
+    if (get_app_permissions(uid) & BPF_PERMISSION_INTERNET) {
+        return bpf_owner_firewall_match(uid) == PASS ? BPF_ALLOW : BPF_DISALLOW;
+    } else {
+        return BPF_DISALLOW;
+    }
 }
 
 DEFINE_NETD_BPF_PROG_KVER("cgroupsockrelease/inet_release", inet_socket_release, KVER_5_10)
