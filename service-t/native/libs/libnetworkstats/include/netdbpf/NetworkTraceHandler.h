a603ce5b8e351f20f8802b6098bc7bc66e46fe87
Ryan Zuklie <rzuklie@google.com>
2023-01-10 14:39:03 -0800

    Add Perfetto part of NetworkTraceHandler
    This adds the DataSource base class, data source registration and
    overrides the lifecycle methods.
    
    Adding the Perfetto SDK increases the size of the tethering apex by
    450kb (~150 KB for the compressed apex).
    
    Bug: 246985031
    Test: atest libnetworkstats_test
    Change-Id: Ie2e8f3e43c8080434408f752346e575a19e9042e

--- service-t/native/libs/libnetworkstats/include/netdbpf/NetworkTraceHandler.h
+++ service-t/native/libs/libnetworkstats/include/netdbpf/NetworkTraceHandler.h
@@ -16,6 +16,9 @@
 
 #pragma once
 
+#include <perfetto/base/task_runner.h>
+#include <perfetto/tracing.h>
+
 #include <string>
 #include <unordered_map>
 
@@ -28,18 +31,38 @@
 namespace android {
 namespace bpf {
 
-class NetworkTraceHandler {
+class NetworkTraceHandler : public perfetto::DataSource<NetworkTraceHandler> {
  public:
-  // Initialize with a callback capable of intercepting data.
+  // Registers this DataSource.
+  static void RegisterDataSource();
+
+  // Initialize with the default Perfetto callback.
+  NetworkTraceHandler();
+
+  // Testonly: initialize with a callback capable of intercepting data.
   NetworkTraceHandler(std::function<void(const PacketTrace&)> callback)
       : mCallback(std::move(callback)) {}
 
-  // Standalone functions without perfetto dependency.
+  // Testonly: standalone functions without perfetto dependency.
   bool Start();
   bool Stop();
   bool ConsumeAll();
 
+  // perfetto::DataSource overrides:
+  void OnSetup(const SetupArgs&) override;
+  void OnStart(const StartArgs&) override;
+  void OnStop(const StopArgs&) override;
+
+  // Convert a PacketTrace into a Perfetto trace packet.
+  void Fill(const PacketTrace& src,
+            ::perfetto::protos::pbzero::TracePacket& dst);
+
  private:
+  void Loop();
+
+  // How often to poll the ring buffer, defined by the trace config.
+  uint32_t mPollMs;
+
   // The function to process PacketTrace, typically a Perfetto sink.
   std::function<void(const PacketTrace&)> mCallback;
 
@@ -48,6 +71,10 @@ class NetworkTraceHandler {
 
   // The packet tracing config map (really a 1-element array).
   BpfMap<uint32_t, bool> mConfigurationMap;
+
+  // This must be the last member, causing it to be the first deleted. If it is
+  // not, members required for callbacks can be deleted before it's stopped.
+  std::unique_ptr<perfetto::base::TaskRunner> mTaskRunner;
 };
 
 }  // namespace bpf
