72b0cd986a6f0fb0a2a6fc7628404082b8b02a7f
Xiao Ma <xiaom@google.com>
2023-06-20 18:49:13 +0900

    Add RouteInformation option Struct.
    This structure is useful when building an RA with RIO for integration
    tests and verify the device's provisioning behavior, for example, check
    if devices in ULA networks can recover its IPv6 connectivity from
    receiving a normal RA after losing IPv6 default route (e.g. received RA
    with zero router lifetime).
    
    Bug: 183601771
    Test: m
    Change-Id: I13b85000ea1633f56edc74e3d62b7a38b9a851bf

--- staticlibs/framework/com/android/net/module/util/NetworkStackConstants.java
+++ staticlibs/framework/com/android/net/module/util/NetworkStackConstants.java
@@ -140,6 +140,7 @@ public final class NetworkStackConstants {
      * ICMPv6 constants.
      *
      * See also:
+     *     - https://tools.ietf.org/html/rfc4191
      *     - https://tools.ietf.org/html/rfc4443
      *     - https://tools.ietf.org/html/rfc4861
      */
@@ -157,6 +158,7 @@ public final class NetworkStackConstants {
     public static final int ICMPV6_ND_OPTION_TLLA  = 2;
     public static final int ICMPV6_ND_OPTION_PIO   = 3;
     public static final int ICMPV6_ND_OPTION_MTU   = 5;
+    public static final int ICMPV6_ND_OPTION_RIO   = 24;
     public static final int ICMPV6_ND_OPTION_RDNSS = 25;
     public static final int ICMPV6_ND_OPTION_PREF64 = 38;
 
