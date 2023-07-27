c0b0f616d0dc094666ac81f81ddda8913ff22ff0
Mark <markchien@google.com>
2023-07-27 08:55:20 +0000

    Random select started prefix range
    Currently, PrivateAddressCoordinator always selects the /24 prefix
    from the 192.168.x.x/16 range first. If the upstream is also a /24
    prefix in the 192.168.x.x/16 range, there's a 1/256 chance of a
    conflict (192.168.0.x/24 ~ 192.168.255.x/24). Since 192.168.x.x/16
    is a popular range for home and small business networks, we should
    randomize the starting prefix range to reduce the chance of a
    conflict. The /8, /12, and /16 range selection rates are 0.39%,
    5.86%, and 93.7%, respectively (see the inline comments for how
    the rates are calculated).
    
    The chance of selecting a /24 prefix is:
    - 10.0.0.0/8: 1/(256*256) * 93.7% = 0.001429%
    - 172.16.0.0/12: 1/(16*256) * 5.86% = 0.00143%
    - 192.168.0.0/16: 1/256 * 0.39% = 0.001523%
    
    Note: This change is currently turned off by the
    tether_force_random_prefix_base_selection flag. We'll run experiments
    to make sure tethering stays reliable with this change enabled.
    
    Bug: 312647670
    Test: atest TetheringTests
    Change-Id: Iea03fdcf0fccad95410e79ae87fcb046d75da457

--- Tethering/src/com/android/networkstack/tethering/PrivateAddressCoordinator.java
+++ Tethering/src/com/android/networkstack/tethering/PrivateAddressCoordinator.java
@@ -187,7 +187,10 @@ public class PrivateAddressCoordinator {
             return cachedAddress;
         }
 
-        for (IpPrefix prefixRange : mTetheringPrefixes) {
+        final int prefixIndex = getStartedPrefixIndex();
+        for (int i = 0; i < mTetheringPrefixes.size(); i++) {
+            final IpPrefix prefixRange = mTetheringPrefixes.get(
+                    (prefixIndex + i) % mTetheringPrefixes.size());
             final LinkAddress newAddress = chooseDownstreamAddress(prefixRange);
             if (newAddress != null) {
                 mDownstreams.add(ipServer);
@@ -198,6 +201,28 @@ public class PrivateAddressCoordinator {
 
         // No available address.
         return null;
+    }
+
+    private int getStartedPrefixIndex() {
+        if (!mConfig.isRandomPrefixBaseEnabled()) return 0;
+
+        final int random = getRandomInt() & 0xffffff;
+        // This is to select the starting prefix range (/8, /12, or /16) instead of the actual
+        // LinkAddress. To avoid complex operations in the selection logic and make the selected
+        // rate approximate consistency with that /8 is around 2^4 times of /12 and /12 is around
+        // 2^4 times of /16, we simply define a map between the value and the prefix value like
+        // this:
+        //
+        // Value 0 ~ 0xffff (65536/16777216 = 0.39%) -> 192.168.0.0/16
+        // Value 0x10000 ~ 0xfffff (983040/16777216 = 5.86%) -> 172.16.0.0/12
+        // Value 0x100000 ~ 0xffffff (15728640/16777216 = 93.7%) -> 10.0.0.0/8
+        if (random > 0xfffff) {
+            return 2;
+        } else if (random > 0xffff) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     private int getPrefixBaseAddress(final IpPrefix prefix) {
