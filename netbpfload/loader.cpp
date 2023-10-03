283c25a07ad5ad7ed7825b7810f701347a53010d
Maciej Żenczykowski <maze@google.com>
2023-10-02 19:43:30 -0700

    netbpfload: update date in copyright messages, LOG_TAGs, etc.
    Test: N/A
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I8f83d04266caf930701b9725b457333ddae149f2

--- netbpfload/loader.cpp
+++ netbpfload/loader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2018-2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "LibBpfLoader"
+#define LOG_TAG "NetBpfLoader"
 
 #include <errno.h>
 #include <fcntl.h>
