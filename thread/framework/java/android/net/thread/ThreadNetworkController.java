75aa485604ec9f3c07a65aba66eaed44c6f04bad
Kangping Dong <wgtdkp@google.com>
2023-10-24 14:18:08 +0800

    [Thread] flagging Android Thread APIs
    This commit declares the initial "thread_enabled" feature flag and use
    this to flag all existing and future Thread system APIs with @FlaggedApi
    
    Bug: 301473012
    Test: build
    Change-Id: I43f64ce38f7fd0806733b21c4f393e4563e6a467

--- thread/framework/java/android/net/thread/ThreadNetworkController.java
+++ thread/framework/java/android/net/thread/ThreadNetworkController.java
@@ -18,6 +18,7 @@ package android.net.thread;
 
 import static java.util.Objects.requireNonNull;
 
+import android.annotation.FlaggedApi;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.SystemApi;
@@ -30,7 +31,8 @@ import java.lang.annotation.RetentionPolicy;
  * Provides the primary API for controlling all aspects of a Thread network.
  *
  * @hide
- */
+*/
+@FlaggedApi(ThreadNetworkFlags.FLAG_THREAD_ENABLED)
 @SystemApi
 public class ThreadNetworkController {
 
