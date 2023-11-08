7b8f475e7912bdc1d55d4818f44ebe72f8605495
Maciej Żenczykowski <maze@google.com>
2023-11-07 17:04:15 -0800

    remove BpfNetMaps.cpp
    Test: builds, TreeHugger
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: If192c17881736c9fe7f8346ad8c5d41d7164eb03

--- service/jni/onload.cpp
+++ service/jni/onload.cpp
@@ -22,7 +22,6 @@
 namespace android {
 
 int register_com_android_server_TestNetworkService(JNIEnv* env);
-int register_com_android_server_BpfNetMaps(JNIEnv* env);
 int register_com_android_server_connectivity_ClatCoordinator(JNIEnv* env);
 int register_android_server_net_NetworkStatsFactory(JNIEnv* env);
 int register_android_server_net_NetworkStatsService(JNIEnv* env);
@@ -39,10 +38,6 @@ extern "C" jint JNI_OnLoad(JavaVM* vm, void*) {
     }
 
     if (android::modules::sdklevel::IsAtLeastT()) {
-        if (register_com_android_server_BpfNetMaps(env) < 0) {
-            return JNI_ERR;
-        }
-
         if (register_com_android_server_connectivity_ClatCoordinator(env) < 0) {
             return JNI_ERR;
         }
