5eab727a2dd255d99b593f2b508c091a83c78d54
Michael Bestas <mkbestas@lineageos.org>
2025-12-13 09:47:36 +0200

    DNM 5.4 BPF hax
    Change-Id: Iea2407e323d5a99495630f877a119259ec82b643

--- bpf/loader/NetBpfLoad.cpp
+++ bpf/loader/NetBpfLoad.cpp
@@ -1635,7 +1635,7 @@ static int doLoad(char** argv, char * const envp[]) {
     // see also: //system/netd/tests/kernel_test.cpp TestKernel510
     if (isAtLeast25Q4 && !isAtLeastKernelVersion(5, 10)) {
         ALOGE("Android 25Q4 requires kernel 5.10.");
-        return 7;
+        //return 7;
     }
 
     // Technically already required by U, but only enforce on V+
